bb2c0a68ae2541a9e7c4fbf53d3bfbffee3717ef
KBModib <kmodibedi@outlook.com>
2022-09-16 16:07:15 +0200

    update

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import TabNavigation from './src/navigation/tabNavigation';
+import Header from './src/components/header';
 
 export default function App() {
   return (
-    
+    <>
+      <Header/>
       <TabNavigation />
-      
+    </>  
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,36 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, View, Image, TouchableOpacity } from 'react-native';
+import React from 'react';
+import logo from '../../assets/Logo.png';
+import { Ionicons } from '@expo/vector-icons';
 
-export default class header extends Component {
-  render() {
-    return (
-      <View>
-        <Text> textInComponent </Text>
+const Header = () => {
+  return (
+      <View style={styles.header}>
+        <Image source={logo} styles={styles.logo} alt={'logo'}/>
+        <TouchableOpacity style={styles.profileIcon}>
+         <Ionicons name="person-circle-sharp" size={80} color="black" />
+        </TouchableOpacity>
       </View>
-    )
-  }
+  )
 }
+
+export default Header
+
+const styles = StyleSheet.create({
+
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 20,
+  },
+  logo: {
+    width: "1px",
+    height: "1px",
+  },
+  profileIcon: {
+    width: 100,
+    height: 100,
+    padding: 10,
+  }
+})
