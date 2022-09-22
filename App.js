6a846932090a5c43ef9a0976f9868bb9917341db
KBModib <kmodibedi@outlook.com>
2022-09-22 15:20:43 +0200

    static screens

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
-import TabNavigation from './src/navigation/tabNavigation';
-import Header from './src/components/header';
+import React from 'react'
+import { Text, View } from 'react-native'
 
 export default function App() {
-  return (
-    <>
-      <Header/>
-      <TabNavigation />
-    </>  
-  );
-}
+     return (
+       <View>
+        <Text>House rules</Text>
+       <Text>List of ques</Text>
+      </View>  
+    );
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import TabNavigation from './src/navigation/stackNavigation';
+// import Header from './src/components/header';
+
+// export default function App() {
+//   return (
+//     <>
+//       <Header/>
+//       <TabNavigation />
+//     </>  
+//   );
+// }
